f9c38411eac0fc23668e3416308044de933c12ce
Shiv mishra <sm096951@gmail.com>
2023-06-09 16:13:42 +0530

    getter setter

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
   public static void main(String[] args){
       RWOnly r=new RWOnly();
-      r.name="Shiv";
-      System.out.print(r.name);
+      r.setter("shiv");
+      r.getter();
   }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,4 +2,10 @@ package com.driver;
 
 public class RWOnly {
     private String name;
+    void setter(String name){
+        this.name=name;
+    }
+    void getter(){
+        System.out.println(this.name);
+    }
 }
