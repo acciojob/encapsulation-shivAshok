11de4d71900ae5c4d6b1661d927ebe484aa1b5c8
Shiv mishra <sm096951@gmail.com>
2023-06-09 16:10:40 +0530

    error getting

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly r=new RWOnly();
+      r.name="Shiv";
+      System.out.print(r.name);
+  }
 }
